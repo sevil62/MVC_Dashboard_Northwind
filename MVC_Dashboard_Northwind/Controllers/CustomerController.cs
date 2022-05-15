553f9d91b6c2b5f2b0bd86169e35af57b55fe345
sevil aydın <sevar2362@gmail.com>
2022-05-16 02:08:52 +0300

    Add project files.

---
+++ MVC_Dashboard_Northwind/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using MVC_Dashboard_Northwind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Dashboard_Northwind.Controllers
+{
+    public class CustomerController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var customer = db.Customers.ToList();
+            ViewBag.Customers = customer;
+            return View();
+        }
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UpdateCustomer(string id)
+        {
+            Customer customer = db.Customers.Find(id);
+            return View(customer);
+        }
+        [HttpPost]
+        public ActionResult UpdateCustomer(Customer customer)
+        {
+            Customer tobeUpdated = db.Customers.Find(customer.CustomerID);
+            db.Entry(tobeUpdated).CurrentValues.SetValues(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(string id)
+        {
+            var customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC_Dashboard_Northwind/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using MVC_Dashboard_Northwind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Dashboard_Northwind.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var employee = db.Employees.ToList();
+            ViewBag.Employees = employee;
+            return View();
+        }
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(Employee employee)
+        {
+            db.Employees.Add(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UpdateEmployee(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            return View(employee);
+        }
+        [HttpPost]
+        public ActionResult UpdateEmployee(Employee employee)
+        {
+            Employee tobeUpdated = db.Employees.Find(employee.EmployeeID);
+            db.Entry(tobeUpdated).CurrentValues.SetValues(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
